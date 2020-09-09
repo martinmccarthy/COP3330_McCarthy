20e3b99826aeb6eccad903b628a5ab732a12c6a4
Martin McCarthy <martinmccarthy@knights.ucf.edu>
2020-09-09 15:15:39 -0400

    test commit

--- p1/src/Decrypter.java
+++ p1/src/Decrypter.java
@@ -1,2 +1,3 @@
-package PACKAGE_NAME;public class Decrypter {
+public class Decrypter {
+
 }
--- p1/src/Encrypter.java
+++ p1/src/Encrypter.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Encrypter {
+public class Encrypter {
+    static int encrypt(String num) {
+        int unencryptedNum = Integer.parseInt(num);
+
+        
+
+        return unencryptedNum;
+    }
+
+    public static void main(String[] args) {
+        int check = encrypt("1234");
+        System.out.println(check);
+    }
 }
