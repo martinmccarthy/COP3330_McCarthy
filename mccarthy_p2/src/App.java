e81ce5834129a394104b103ee142fd44d915d509
Martin McCarthy <martinmccarthy@knights.ucf.edu>
2020-09-28 16:25:02 -0400

    updates app and bodymassindex files

--- mccarthy_p2/src/App.java
+++ mccarthy_p2/src/App.java
@@ -1,2 +1,87 @@
-package PACKAGE_NAME;public class App {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App {
+    double bmiTotal = 0;
+
+    private static boolean moreInput() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter more input? Y/N: ");
+
+        String truthCheck = scan.next();
+
+        boolean truthValue = false;
+
+        if(truthCheck.equals("Y")) {
+            truthValue = true;
+        } else if(truthCheck.equals("N")) {
+            truthValue = false;
+        }
+
+        return truthValue;
+    }
+
+    private static double getUserHeight() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a height (in inches): ");
+
+        double height = scan.nextDouble();
+        scan.nextLine();
+
+        while(height < 0) {
+            System.out.println("Invalid input, re-enter height: ");
+
+            height = scan.nextDouble();
+            scan.nextLine();
+        }
+
+        return height;
+    }
+
+    private static double getUserWeight() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a weight (in pounds): ");
+
+        double weight = scan.nextDouble();
+        scan.nextLine();
+
+        while(weight < 0) {
+            System.out.println("Invalid input, re-enter weight: ");
+
+            weight = scan.nextDouble();
+            scan.nextLine();
+        }
+
+        return weight;
+    }
+
+    static void displayBmiInfo(BodyMassIndex bmi) {
+        double bmiScore = bmi.calculateBmi();
+        String bmiCategory = bmi.calculateBmiCategory();
+
+        System.out.println("User's BMI: " + bmiScore + ", User is: " + bmiCategory);
+    }
+
+    static void displayBmiStatistics(ArrayList bmiData) {
+
+    }
+
+    public static void main(String[] args) {
+        ArrayList<BodyMassIndex> bmiData = new ArrayList<BodyMassIndex>();
+
+        while (moreInput()) {
+            double height = getUserHeight();
+            double weight = getUserWeight();
+
+            BodyMassIndex bmi = new BodyMassIndex(height, weight);
+            bmiData.add(bmi);
+
+            displayBmiInfo(bmi);
+        }
+
+        displayBmiStatistics(bmiData);
+    }
 }
--- mccarthy_p2/src/BodyMassIndex.java
+++ mccarthy_p2/src/BodyMassIndex.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class BodyMassIndex {
+import static java.lang.Math.pow;
+
+public class BodyMassIndex {
+    private double height;
+    private double weight;
+
+
+    public BodyMassIndex(double height, double weight) {
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public double calculateBmi() {
+        return (703 * weight) / pow(height, 2);
+    }
+
+    public String calculateBmiCategory() {
+        String bmiCategory;
+        double bmi = calculateBmi();
+
+        if(bmi < 18.5) {
+            bmiCategory = "Underweight";
+        }
+        else if(bmi >= 18.5 && bmi < 25) {
+            bmiCategory = "Normal weight";
+        }
+        else if(bmi >= 25 && bmi < 30) {
+            bmiCategory = "Overweight";
+        }
+        else {
+            bmiCategory = "Obesity";
+        }
+
+        return bmiCategory;
+    }
 }
