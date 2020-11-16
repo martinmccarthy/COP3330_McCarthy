9bf42cdc2b5cae0e1c04f12d2eb5d0016d259343
Martin McCarthy <martinmccarthy@knights.ucf.edu>
2020-11-16 17:17:59 -0500

    adds test cases for taskitem.java

--- mccarthy_p4/src/TaskItemTest.java
+++ mccarthy_p4/src/TaskItemTest.java
@@ -3,6 +3,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class TaskItemTest {
+    //Title tests
     @Test
     void creatingTaskItemFailsWithInvalidTitle() {
         TaskItem item = new TaskItem();
@@ -14,4 +15,23 @@ public class TaskItemTest {
         TaskItem item = new TaskItem();
         assertEquals(0, item.titleCheck("new task"));
     }
+
+    @Test
+    void settingTaskItemFailsWithValidTitle() {
+        TaskItem item = new TaskItem();
+        item.setTitle("");
+
+        assertEquals(1, item.titleCheck(item.getTitle()));
+    }
+
+    @Test
+    void settingTaskItemSucceedsWithValidTitle() {
+        TaskItem item = new TaskItem();
+        item.setTitle("new task");
+
+        assertEquals(0, item.titleCheck(item.getTitle()));
+    }
+
+    //DueDate Tests
+
 }
